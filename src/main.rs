1eecf79ece4be66d377a293c3d8f52e9e1a75424
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2017-06-04 15:51:37 +0200

    Restructure modules

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,17 @@
 extern crate lazy_static;
 extern crate regex;
 
-use std::thread;
-use std::sync::mpsc::{channel, Sender};
-use std::path::Path;
 use std::fs::File;
-use std::io;
-use std::io::prelude::*;
 use std::io::SeekFrom;
+use std::io::prelude::*;
+use std::io;
+use std::path::Path;
+use std::sync::mpsc::{channel, Sender};
+use std::thread;
 use std::time::Duration;
 
 mod models;
+mod parsers;
 
 fn init_log() -> io::Result<()> {
     let log_config = b"[Achievements]
@@ -35,60 +36,23 @@ ScreenPrinting=false";
     Ok(())
 }
 
-fn tail_log(tx: Sender<models::Play>) -> io::Result<()> {
+fn tail_log(tx: Sender<parsers::LogEvent>) -> io::Result<()> {
     let hearthstone_path = Path::new(r"C:\Program Files (x86)\Hearthstone\Logs\Power.log");
     let mut handle = io::BufReader::new(File::open(hearthstone_path)?);
     handle.seek(SeekFrom::End(0))?;
 
     loop {
-        let mut b = String::new();
-        match handle.read_line(&mut b) {
+        let mut buffer = String::new();
+        match handle.read_line(&mut buffer) {
             Ok(0) => {
                 thread::sleep(Duration::from_millis(250));
             }
             Ok(_) => {
-                match parse_log_line(&b) {
-                    Some(play) => tx.send(play).unwrap(),
-                    _ => (),
-                }
+                parsers::parse_log_line(&buffer).map(|play| tx.send(play).unwrap());
             }
             Err(err) => println!("Error!, {}", err),
         }
     }
-}
-
-fn parse_log_line(line: &str) -> Option<models::Play> {
-    lazy_static! {
-        static ref CARD_UPDATE_PATTERN: regex::Regex = regex::Regex::new(
-            r"^.*id=(?P<id>\d*) .*cardId=(?P<card_id>[a-zA-Z0-9_]*) .*player=(?P<player>\d*)")
-                .unwrap();
-        static ref GAME_COMPLETE_PATTERN: regex::Regex = regex::Regex::new(
-            r"^.*TAG_CHANGE Entity=GameEntity tag=STATE value=COMPLETE.*$")
-                .unwrap();
-    }
-
-    if GAME_COMPLETE_PATTERN.is_match(line) {
-        return None;
-    }
-
-    CARD_UPDATE_PATTERN
-        .captures(line)
-        .and_then(|group| {
-            let id = group.name("id").map(|m| m.as_str());
-            let card_id = group.name("card_id").map(|m| m.as_str());
-            let player = group.name("player").map(|m| m.as_str());
-
-            match (id, card_id, player) {
-                (Some(id), Some(card_id), Some(player)) if card_id != "" => {
-                    Some(models::Play {
-                             id: id.to_string(),
-                             card_id: card_id.to_string(),
-                             player: player.to_string(),
-                         })
-                }
-                _ => None,
-            }
-        })
 }
 
 fn main() {
@@ -106,10 +70,18 @@ fn main() {
 
     let mut game_state = models::GameState::default();
     while let Ok(play) = rx.recv() {
-        let updated = game_state.handle_play(play);
-        if updated {
-            println!("New state: {:?}", game_state);
-            println!();
+        match play {
+            parsers::LogEvent::GameComplete => {
+                print!("Game completed");
+                game_state = models::GameState::default();
+            }
+            parsers::LogEvent::Play(play) => {
+                let updated = game_state.handle_play(play);
+                if updated {
+                    println!("New state: {:?}", game_state);
+                    println!();
+                }
+            }
         }
     }
 }
---
+++ src/parsers.rs
@@ -0,0 +1,59 @@
+use regex;
+use models;
+
+lazy_static! {
+    static ref CARD_UPDATE_PATTERN: regex::Regex = regex::Regex::new(
+        r"^.*id=(?P<id>\d*) .*cardId=(?P<card_id>[a-zA-Z0-9_]*) .*player=(?P<player>\d*)")
+            .unwrap();
+    static ref GAME_COMPLETE_PATTERN: regex::Regex = regex::Regex::new(
+        r"^.*TAG_CHANGE Entity=GameEntity tag=STATE value=COMPLETE.*$")
+            .unwrap();
+}
+
+#[derive(Debug)]
+pub enum LogEvent {
+    GameComplete,
+    Play(models::Play),
+}
+
+pub fn parse_log_line(line: &str) -> Option<LogEvent> {
+    if GAME_COMPLETE_PATTERN.is_match(line) {
+        return Some(LogEvent::GameComplete);
+    }
+
+    CARD_UPDATE_PATTERN
+        .captures(line)
+        .and_then(|group| {
+            let id = group.name("id").map(|m| m.as_str());
+            let card_id = group.name("card_id").map(|m| m.as_str());
+            let player = group.name("player").map(|m| m.as_str());
+
+            match (id, card_id, player) {
+                (Some(id), Some(card_id), Some(player)) if card_id != "" => {
+                    Some(LogEvent::Play(models::Play {
+                                            id: id.to_string(),
+                                            card_id: card_id.to_string(),
+                                            player: player.to_string(),
+                                        }))
+                }
+                _ => None,
+            }
+        })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_game_complete() {
+        let log_line = r"D 15:36:19.5943367 PowerTaskList.DebugPrintPower() -     TAG_CHANGE Entity=GameEntity tag=STATE value=COMPLETE";
+        assert!(GAME_COMPLETE_PATTERN.is_match(log_line));
+    }
+
+    #[test]
+    fn test_card_update() {
+        let log_line = r"D 14:50:27.0664788 GameState.DebugPrintEntityChoices() -   Entities[4]=[name=La pièce id=68 zone=HAND zonePos=5 cardId=GAME_005 player=1]";
+        assert!(CARD_UPDATE_PATTERN.is_match(log_line));
+    }
+}
